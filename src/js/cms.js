2f602493767a75c149ea5db594151092a7e740db
AsarMichil <asarmichil@gmail.com>
2022-06-25 15:49:06 -0700

    cms work>

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,7 +3,17 @@ import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+import HomePreview from "./cms-preview-templates/home";
+import PostPreview from "./cms-preview-templates/post";
+import ProductsPreview from "./cms-preview-templates/products";
+import BooksPreview from "./cms-preview-templates/books";
+import ContactPreview from "./cms-preview-templates/contact";
 
-CMS.registerPreviewStyle(styles, { raw: true });
 
 CMS.init();
+CMS.registerPreviewStyle(styles, { raw: true });
+// CMS.registerPreviewTemplate("home", HomePreview);
+// CMS.registerPreviewTemplate("post", PostPreview);
+// CMS.registerPreviewTemplate("products", ProductsPreview);
+// CMS.registerPreviewTemplate("books", BooksPreview);
+// CMS.registerPreviewTemplate("contact", ContactPreview);
